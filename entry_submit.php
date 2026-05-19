7feecf873ac05e1f15e53ce812c3eeedc91f1014
105921779 <105921779@student.swin.edu.au>
2026-05-19 11:35:49 +1000

    modified:   entry_index.html 	modified:   entry_submit.php

--- entry_submit.php
+++ entry_submit.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 } else {
 
-    header("Location: index.html");
+    header("Location: entry_index.html");
     exit();
 }
 
@@ -50,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         </div>
 
-        <a href="index.html" class="back-button">
+        <a href="entry_index.html" class="back-button">
             Enter Another Score
         </a>
 
