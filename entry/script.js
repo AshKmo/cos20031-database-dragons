410471a2f766dc44ffcbe8c07af0a723f488ed6a
105921779 <105921779@student.swin.edu.au>
2026-05-21 13:08:30 +1000

    modified:   entry/script.js

--- entry/script.js
+++ entry/script.js
@@ -12,15 +12,9 @@ function updateArcherInfo() {
     const lastName =
         selectedOption.getAttribute("data-last");
 
-    const equipment =
-        selectedOption.getAttribute("data-equipment");
-
     document.getElementById("firstName").value =
         firstName || "";
 
     document.getElementById("lastName").value =
         lastName || "";
-
-    document.getElementById("equipment").value =
-        equipment || "Recurve";
 }
